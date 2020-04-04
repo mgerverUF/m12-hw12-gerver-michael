d311170166d6bc71986e4b310b35b94c0bbbe45c
Michael Gerver <mgerver@ufl.edu>
2020-04-04 17:00:16 -0400

    Add randomization function and method to output first array item

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 const candy = ['Kit Kat','M&Ms','Skittles','Gummy Bears','Milky Way','Snickers','Starburst','Crunch','Twix','Twizzlers'];
+
+
+document.getElementById('result').innerHTML = candy[0];
+let randNum = (i, j, k) => {
+    for (i = candy.length -1; i > 0; i--) {
+        j = Math.floor(Math.random() * i)
+        k = candy[i]
+        candy[i] = candy[j]
+        candy[j] = k
+      };
+}; 
