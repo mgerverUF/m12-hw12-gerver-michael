6e7f4e7700c9f0450adfffc6627dd7d7f87d4d29
Michael Gerver <mgerver@ufl.edu>
2020-04-04 18:43:14 -0400

    Complete javascript code functionality

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
-// Candy array
-const candy = ['Kit Kat','M&Ms','Skittles','Gummy Bears','Milky Way','Snickers','Starburst','Crunch','Twix','Twizzlers'];
-
-// Function to shuffle elements within the array
-let shuffleNum = (i, j, k) => {
-    for (i = candy.length -1; i > 0; i--) {
-        j = Math.floor(Math.random() * i)
-        k = candy[i]
-        candy[i] = candy[j]
-        candy[j] = k
-        document.getElementById('result').innerHTML = candy[j];
-      };
-}; 
-
-// Feeds candy array to shuffleNum function
-shuffleNum(candy);
+// Creates reference to button in HTML document
+const btn = document.getElementById('candy-button');
 
 // Adds functionality to button 
+// https://medium.com/@josephcardillo/arrow-functions-and-this-in-es6-4f1d350a85cf
+btn.addEventListener('click', () => {
+
+    // Candy array
+    const candy = ['Kit Kat','M&Ms','Skittles','Gummy Bears','Milky Way','Snickers','Starburst','Crunch','Twix','Twizzlers'];
+
+    // Function to shuffle elements within the array
+    let shuffleNum = (i, j, k) => {
+        for (i = candy.length -1; i > 0; i--) {
+            j = Math.floor(Math.random() * i)
+            k = candy[i]
+            candy[i] = candy[j]
+            candy[j] = k
+            document.getElementById('result').innerHTML = candy[j];
+        };
+    }; 
+
+    // Feeds candy array to shuffleNum function
+    shuffleNum(candy);
+  });
 
